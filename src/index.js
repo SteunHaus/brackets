84430b32e2b50d2cd03a22a194853b00f8fc3c58
SteunHaus <pavel.evdokimenko@gmail.com>
2021-03-13 00:52:07 +0300

    fix: Update index.js with valid solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketString = '';
+  let lengthCheck = 0;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    lengthCheck = str.length;
+    bracketString = bracketsConfig[i][0] + bracketsConfig[i][1];
+    while (str.includes(bracketString)) str = str.replace(bracketString, '');
+    if (str.length != lengthCheck && str.length != 0) i = -1;
+  }
+  if (str.length == 0) return true;
+  else return false;
 }
